5cd04cecc48d8283213a5b9bf87db5528d4fc4dc
SavioFagundes <akirasfm@gmail.com>
2025-04-28 19:36:09 -0300

    Firt Commit

--- UsuarioLib/Program.cs
+++ UsuarioLib/Program.cs
@@ -1,23 +1,32 @@
-﻿using UsuarioLib;
+﻿using System.Diagnostics;
+using UsuarioLib;
 
-Usuario usuario = 
-    new Usuario(
-        "Daniel", 
-        "daniel@email.com",
-        new List<string>() {"12345678"});
+//Usuario usuario = 
+//    new Usuario(
+//        "Daniel", 
+//        "daniel@email.com",
+//        new List<string>() {"12345678"});
 
-Usuario usuario2 =
-    new Usuario(
-        "Luis",
-        "luis@email.com",
-        new List<string>() { "87654321" });
+////12345678
+//usuario.ExibeTelefones();
 
+////efetuar a padronização
+//usuario.PadronizaTelefones();
 
-//12345678
-usuario.ExibeTelefones();
+////912345678
+//usuario.ExibeTelefones();
 
-//efetuar a padronização
-usuario.PadronizaTelefones();
+Stopwatch sw = new Stopwatch();
 
-//912345678
-usuario.ExibeTelefones();
+sw.Start();
+
+for (int i = 0; i < 1000000000; i++)
+{
+    Coordenada coordenada = new Coordenada(123.456, -123.456);
+    var latitude = coordenada.Latitude;
+    var longitude = coordenada.Longitude;
+}
+
+sw.Stop();
+
+Console.WriteLine(sw.Elapsed.TotalMilliseconds);
